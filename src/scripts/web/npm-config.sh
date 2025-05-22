be6456c7e010a719fd8df915d3d23e915ce2693f
Rehan Haider <email@rehanhaider.com>
2025-05-22 17:46:24 +0000

    FEAT:  npm script for web

--- src/scripts/web/npm-config.sh
+++ src/scripts/web/npm-config.sh
@@ -2,8 +2,14 @@
 
 
 npm init -y
-npm pkg set name=aws version=0.1.0
+npm pkg set name=web version=0.1.0
 npm pkg delete description main keywords author license type
-npm pkg set scripts.build=tsc scripts.test=jest scripts.watch='tsc -w' scripts.cdk=cdk
-npm install --save aws-cdk-lib constructs
-npm install --save-dev typescript @types/node @types/jest jest ts-node ts-jest aws-cdk
+npm pkg set scripts.dev='astro dev' scripts.start='astro dev' scripts.build='astro check && astro build'
+npm pkg set scripts.preview='astro preview' scripts.astro='astro'
+npm install --save astro @astrojs/react @astrojs/sitemap
+npm install --save react react-dom react-router-dom @types/react @types/react-dom
+npm install --save @aws-sdk/client-cognito-identity-provider
+npm install --save @tanstack/query-sync-storage-persister @tanstack/react-query @tanstack/react-query-persist-client
+npm install --save nanostores @nanostores/persistent @nanostores/react
+npm install --save axios marked tailwindcss @tailwindcss/vite typescript
+npm install --save-dev daisyui prettier prettier-plugin-astro prettier-plugin-tailwindcss @tanstack/eslint-plugin-query
